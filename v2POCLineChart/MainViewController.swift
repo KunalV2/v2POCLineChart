4ba41f8af8b91d4db4eb2aea0e4125e905c607ed
KunalV2 <kunal.parekh@v2solutions.com>
2017-10-13 11:52:35 +0530

    Added functionality of Line chart and made customisation in chart -  remove y-Axis Label , Change colour of x-Axis Label, skip points whoes data is not available

---
+++ v2POCLineChart/MainViewController.swift
@@ -0,0 +1,82 @@
+
+import UIKit
+import QuartzCore
+
+class MainViewController: UIViewController, LineChartDelegate {
+
+    
+    
+    var label = UILabel()
+    var lineChart: LineChart!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var views: [String: AnyObject] = [:]
+        
+        label.text = "..."
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = NSTextAlignment.center
+        self.view.addSubview(label)
+        views["label"] = label
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[label]-|", options: [], metrics: nil, views: views))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-80-[label]", options: [], metrics: nil, views: views))
+        
+        // simple arrays
+        
+      //  let data: [CGFloat] = [0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0]
+        
+        let data: [CGFloat] = [3, 4, 2, 11, 0, 15,3, 0, 2, 11, 13, 15]
+      //  let data2: [CGFloat] = [1, 3, 5, 13, 17, 20]
+        
+        // simple line with custom x axis labels
+        let xLabels: [String] = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "July", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        
+        lineChart = LineChart()
+        lineChart.animation.enabled = true
+        lineChart.area = true
+        lineChart.x.labels.visible = true
+        lineChart.x.grid.count = 1
+        lineChart.y.grid.count = 1
+        lineChart.x.labels.values = xLabels
+        lineChart.y.labels.visible = false
+        lineChart.addLine(data)
+      //  lineChart.addLine(data2)
+        
+        lineChart.translatesAutoresizingMaskIntoConstraints = false
+        lineChart.delegate = self
+        self.view.addSubview(lineChart)
+        views["chart"] = lineChart
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[chart]-|", options: [], metrics: nil, views: views))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[label]-[chart(==100)]", options: [], metrics: nil, views: views))        
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    
+    /**
+     * Line chart delegate method.
+     */
+    func didSelectDataPoint(_ x: CGFloat, yValues: Array<CGFloat>) {
+        label.text = "x: \(x)     y: \(yValues)"
+    }
+    
+    
+    
+    /**
+     * Redraw chart on device rotation.
+     */
+    override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
+        if let chart = lineChart {
+            chart.setNeedsDisplay()
+        }
+    }
+
+}
